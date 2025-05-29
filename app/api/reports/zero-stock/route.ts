f7fc3ce3d597e1981ba72498c9298e382b85ef62
Mohammad Yosef <mohmd.sy2002@gmail.com>
2025-05-29 22:19:11 +0300

    version 2 remake fix bug

--- app/api/reports/zero-stock/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import Product from "@/lib/models/Product";
-import { connectToDB } from "@/lib/mongoDB";
-
-export async function GET() {
-  await connectToDB();
-  const zeroStockItems = await Product.find({ quantity: { $lte: 0 } });
-  return NextResponse.json(zeroStockItems);
-}
